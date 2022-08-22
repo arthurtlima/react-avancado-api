3dd5635ce018dfaee0a796f80cb20bb173b0dd84
Arthur Lima <arthur.alves.lima@live.com>
2022-08-22 00:54:37 -0300

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8aa32d908c6af03e4ccfc482aebd8fee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ec2993d-231c-4a42-bf2b-12a865da1124'
+};
